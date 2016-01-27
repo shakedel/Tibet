bcd0f6e435471618115f5cbf0108b2816d34ab05
Elad Shaked <shakedel@gmail.com>
2016-01-27 21:54:58 +0200

    removed everything!

--- project/TryAPBT/src/ConcurrentCounter.java
+++
@@ -1,7 +0,0 @@
-public class ConcurrentCounter {
-    private int value;
-
-    public synchronized int get() { return value; }
-    public synchronized int increment() { return ++value; }
-    public synchronized int decrement() { return --value; }
-}
